b259fb6abd86e15cf497b84aebbea6dccc297038
MGops <164943226+MGops@users.noreply.github.com>
2025-06-13 22:51:55 +0100

    Added buttons, layout changes
    Added a button Grid panel and cleaned up the layout a bit more

--- TabDash.java
+++ TabDash.java
@@ -1,5 +1,10 @@
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -23,20 +28,40 @@ public class TabDash {
         mainPanel.add(panelNorth, BorderLayout.NORTH);
 
         JPanel panelWest = new JPanel();
-        panelWest.setBackground(Color.LIGHT_GRAY);
         panelWest.setPreferredSize(new Dimension(200,500));
+        panelWest.setLayout(new BoxLayout(panelWest, BoxLayout.Y_AXIS));
+        panelWest.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         mainPanel.add(panelWest, BorderLayout.WEST);
         
-        JPanel panelCenter = new JPanel();
-        panelCenter.setBackground(Color.WHITE);
-        mainPanel.add(panelCenter, BorderLayout.CENTER);
-
-        frame.setVisible(true);
-
+        JLabel listLabel = new JLabel("Names");
+        listLabel.setAlignmentX(Component.CENTER_ALIGNMENT);;
+        panelWest.add(listLabel);
+        
         DefaultListModel nameListModel = new DefaultListModel<>();
         JList nameList = new JList<>(nameListModel);
         JScrollPane nameScrollPane = new JScrollPane(nameList);
-        nameScrollPane.setBounds(5, 20, 190, 300);
+        nameListModel.addElement("Tom");
+        nameScrollPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, 300));
+        nameScrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelWest.add(nameScrollPane);
+
+        JPanel panelCenter = new JPanel();
+        panelCenter.setBackground(Color.WHITE);
+        panelCenter.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        mainPanel.add(panelCenter, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 5, 0));
+        panelWest.add(Box.createRigidArea(new Dimension(0, 10)));
+        buttonPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));;
+        
+        JButton addButton = new JButton("Add");
+        buttonPanel.add(addButton);        
+
+        JButton removeButton = new JButton("Remove");
+        buttonPanel.add(removeButton);
+        panelWest.add(buttonPanel);
+
+        frame.setVisible(true);
+
     }
 }

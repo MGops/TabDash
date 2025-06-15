85b162f6ddd33c8ee60d4f5670ec8a9236bb1608
MGops <164943226+MGops@users.noreply.github.com>
2025-06-15 22:54:22 +0100

    Changed Header panel to GridBagLayout
    Major changes to header panel to arrange elements properly using GridbagLayout from what was previously GridLayout.

--- HeaderPanel.java
+++ HeaderPanel.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -10,37 +10,59 @@ import javax.swing.JPanel;
 public class HeaderPanel extends JPanel{
     public HeaderPanel() {
         setPreferredSize(new Dimension(800, 80));
-        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        setLayout(new GridLayout(2,4, 10, 0));
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        setLayout(new GridBagLayout());
         setBackground(Color.DARK_GRAY);
 
+        GridBagConstraints coordinates = new GridBagConstraints();
+        coordinates.fill = GridBagConstraints.BOTH;
+        coordinates.weighty = 1;
+
         JLabel initialsLabel = new JLabel("Placeholder");
+        coordinates.gridx = 0;
+        coordinates.gridy = 0;
+        coordinates.gridwidth = 4;
+        coordinates.weightx = 1;
         initialsLabel.setForeground(Color.WHITE);
-        add(initialsLabel);
+        add(initialsLabel, coordinates);
 
         JLabel careCoLabel = new JLabel("Care coordinator");
+        coordinates.gridx = 4;
+        coordinates.gridy = 0;
+        coordinates.gridwidth = 4;
+        coordinates.weightx = 1;
         careCoLabel.setForeground(Color.WHITE);
-        add(careCoLabel);
-
-        JLabel blankLabel1 = new JLabel("");
-        add(blankLabel1);
-
+        add(careCoLabel, coordinates);
+                
         JLabel resusLabel = new JLabel("Resus status");
+        coordinates.gridx = 8;
+        coordinates.gridy = 0;
+        coordinates.gridwidth = 1;
+        coordinates.weightx = 0;
         resusLabel.setForeground(Color.WHITE);
-        add(resusLabel);
+        add(resusLabel, coordinates);
 
         JLabel namedNurseLabel = new JLabel("Named nurse:");
+        coordinates.gridx = 0;
+        coordinates.gridy = 1;
+        coordinates.gridwidth = 4;
+        coordinates.weightx = 1;
         namedNurseLabel.setForeground(Color.WHITE);
-        add(namedNurseLabel);
+        add(namedNurseLabel, coordinates);
 
         JLabel cmhtLabel = new JLabel("CMHT: ");
+        coordinates.gridx = 4;
+        coordinates.gridy = 1;
+        coordinates.gridwidth = 4;
+        coordinates.weightx = 1;
         cmhtLabel.setForeground(Color.WHITE);
-        add(cmhtLabel);
-
-        JLabel blankLabel2 = new JLabel("");
-        add(blankLabel2);
-
+        add(cmhtLabel, coordinates);
+        
         JButton editButton = new JButton("Edit");
-        add(editButton);
+        coordinates.gridx = 8; 
+        coordinates.gridy = 1;
+        coordinates.gridwidth = 1;
+        coordinates.weightx = 0;
+        add(editButton, coordinates);
     }
 }

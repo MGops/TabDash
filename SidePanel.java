b0abfbf8b5f1cc7f9dd40b0ebef13045c1be2ceb
MGops <164943226+MGops@users.noreply.github.com>
2025-06-16 22:19:13 +0100

    added sample patients to sidebar

--- SidePanel.java
+++ SidePanel.java
@@ -13,9 +13,27 @@ public class SidePanel extends JPanel{
         add(listLabel);
 
         DefaultListModel<String> nameListModel = new DefaultListModel<>();
-        nameListModel.addElement("Tom");
-        nameListModel.addElement("Harry");
-        nameListModel.addElement("Richard");
+            
+        nameListModel.addElement("AB 123456");
+        nameListModel.addElement("CD 678907");
+        nameListModel.addElement("EF 234561");
+        nameListModel.addElement("GH 789012");
+        nameListModel.addElement("IJ 345678");
+        nameListModel.addElement("KL 890123");
+        nameListModel.addElement("MN 456789");
+        nameListModel.addElement("OP 901234");
+        nameListModel.addElement("QR 567890");
+        nameListModel.addElement("ST 012345");
+        nameListModel.addElement("UV 678901");
+        nameListModel.addElement("WX 234567");
+        nameListModel.addElement("YZ 789012");
+        nameListModel.addElement("AC 345678");
+        nameListModel.addElement("BD 890123");
+        nameListModel.addElement("CE 456789");
+        nameListModel.addElement("DF 901234");
+        nameListModel.addElement("EG 567890");
+        nameListModel.addElement("FH 012345");
+        nameListModel.addElement("GI 678901");
 
         JList<String> nameList = new JList<>(nameListModel);
         JScrollPane nameScrollPane = new JScrollPane(nameList);

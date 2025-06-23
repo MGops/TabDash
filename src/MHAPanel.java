85088913e90f7d56a9b06ad610739081f24101d5
MGops <164943226+MGops@users.noreply.github.com>
2025-06-23 20:46:32 +0100

    Created Layout of MHA tab

--- src/MHAPanel.java
+++ src/MHAPanel.java
@@ -1,6 +1,51 @@
 package src;
-import javax.swing.JPanel;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.*;
 
 public class MHAPanel extends JPanel{
+    private TabDash tabDash;
+
+    public MHAPanel(TabDash tabDash) {
+        this.tabDash = tabDash;
+        setLayout(new BorderLayout());
+        initialiseComponents();
+    }
+
+    private void initialiseComponents() {
+        JPanel topSection = createTopSection();
+        JPanel middleSection = createMiddleSection();
+        JPanel bottomSection = createBottomSection();
+
+        JPanel mainContainer = new JPanel();
+        mainContainer.setLayout(new BoxLayout(mainContainer, BoxLayout.Y_AXIS));
+
+        mainContainer.add(topSection);
+        mainContainer.add(createSeparator());
+        mainContainer.add(middleSection);
+        mainContainer.add(createSeparator());
+        mainContainer.add(bottomSection);
     
+        add(mainContainer, BorderLayout.CENTER);
+    }
+
+    private JSeparator createSeparator() {
+        JSeparator separator = new JSeparator(JSeparator.HORIZONTAL);
+        separator.setMaximumSize(new Dimension(Integer.MAX_VALUE, 2));
+        return separator;
+    }
+
+    private JPanel createTopSection() {
+        return new JPanel();
+    }
+
+    private JPanel createMiddleSection() {
+            return new JPanel();
+        }
+
+    private JPanel createBottomSection() {
+            return new JPanel();
+        }
 }

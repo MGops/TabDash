d784fb55171be0f1a99477d9cabf977445d4e2a5
MGops <164943226+MGops@users.noreply.github.com>
2025-06-27 23:56:18 +0100

    Added MHA fields to Patient Class

--- src/Patient.java
+++ src/Patient.java
@@ -1,10 +1,28 @@
 package src;
 
 import java.util.HashMap;
+import java.util.Date;
 
 public class Patient {
     private String patientId;
     private HashMap<String, Integer> medications;
+    private boolean mh03Completed;
+    private String sectionStatus;
+    private Date admissionDate;
+    private Date detentionDate;
+    private Date originalDetentionDate;
+    private String capacity;
+    private boolean soadRequested;
+    private Date soadDate;
+    private String soadReference;
+    private boolean s62Completed;
+    private Date s62Date;
+    private boolean t3Provided;
+    private Date t3Date;
+    private Date t3ReviewDate;
+    private boolean t2Completed;
+    private Date t2Date;
+    private Date t2ReviewDate;
    
     public Patient(String patientId) {
         this.patientId = patientId;

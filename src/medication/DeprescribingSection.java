3161368b46cc43a99972be720e0784eed0a2ae28
MGops <164943226+MGops@users.noreply.github.com>
2025-07-08 00:30:51 +0100

    Changed layout of column
    To make med columns start at the top of the panel

--- src/medication/DeprescribingSection.java
+++ src/medication/DeprescribingSection.java
@@ -33,11 +33,11 @@ public class DeprescribingSection extends JPanel {
 
         // Create main panel for columns
         columnsPanel = new JPanel();
-        columnsPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5,0));
+        columnsPanel.setLayout(new BoxLayout(columnsPanel, BoxLayout.X_AXIS));
 
         JScrollPane scrollPane = new JScrollPane(columnsPanel);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
         add(scrollPane, BorderLayout.CENTER);
 
         analyseCurrentPatientDeprescribing();
@@ -66,7 +66,11 @@ public class DeprescribingSection extends JPanel {
         for (DeprescribingCategory category : DeprescribingCategory.values()) {
             List<String> medsInCategory = categoryMedications.get(category);
             if(medsInCategory != null && !medsInCategory.isEmpty()) {
+                if (hasAnyMeds) {
+                    columnsPanel.add(Box.createHorizontalStrut(5));
+                }
                 JPanel columnPanel = createColumnPanel(category, medsInCategory);
+                columnPanel.setAlignmentY(Component.TOP_ALIGNMENT);
                 columnsPanel.add(columnPanel);
                 hasAnyMeds = true;
             }
